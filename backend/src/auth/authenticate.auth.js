048604b7a13f3023821b50b18d17c703805b6273
unknown <nelsondiasdemedeiros@gmail.com>
2020-01-02 13:11:27 -0200

    feature: authenticateAuth student e teacher

--- backend/src/auth/authenticate.auth.js
+++ backend/src/auth/authenticate.auth.js
@@ -1,5 +1,6 @@
 import teacherRepository from '../repositories/teacher.repository';
 import studentRepository from '../repositories/student.repository';
+import { LOGIN_EXPIRATION_TIME } from '../auth/config';
 import Token from './token.auth';
 import hash from './hash.auth';
 
@@ -10,8 +11,48 @@ class AuthenticateAuth {
         const student =  await studentRepository.findByIndentity(data.identity);
         const teacher = await teacherRepository.findByEmail(data.email);
         
-        
-        
-    }
+        if (!student) { throw new Error('ERR_STUDENT_NOT_FOUND'); }
+        if (!teacher) { throw new Error('ERR_TEACHER_NOT_FOUND'); }
 
+        if (student) {
+            const passwordOk = await hash.compare(data.password, student.password);
+
+            if (!passwordOk) { throw new Error('ERR_INVALID_PASSWORD'); }
+
+            const JWTData = {
+                exp: Math.floor(Date.now() / 1000) + LOGIN_EXPIRATION_TIME,
+                info: 'api',
+                data: {
+                  student_id: student.student_id,
+                  student_name: student.name
+                }
+            };
+
+            const token = await Token.generate(JWTData);
+
+            return { student, token };
+        }
+
+        if (teacher) {
+
+            const passwordOk = await hash.compare(data.password, teacher.password);
+
+            if (!passwordOk) { throw new Error('ERR_INVALID_PASSWORD'); }
+
+            const JWTData = {
+                exp: Math.floor(Date.now() / 1000) + LOGIN_EXPIRATION_TIME,
+                info: 'api',
+                data: {
+                  teacher_id: teacher.teacher_id,
+                  teacher_name: teacher.name
+                }
+            };
+
+            const token = await Token.generate(JWTData);
+
+            return { teacher, token };
+        }
+    }
 }
+
+export default new AuthenticateAuth();
