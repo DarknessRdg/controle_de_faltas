d9c05fc58a848c6b106c9704f0f589230f9e9e4d
unknown <nelsondiasdemedeiros@gmail.com>
2020-01-01 22:34:31 -0200

    feature: create Teacher

--- backend/src/handlers/teacher.handler.js
+++ backend/src/handlers/teacher.handler.js
@@ -1,9 +1,11 @@
-import teacherRepository from '../repositories/teacher.repository'
+import teacherRepository from '../repositories/teacher.repository';
 
 class TeacherHandler {
 
     async store(req, res) {
-        
+        const teacher = await teacherRepository.create(req.body);
+        return res.status(201).json(teacher);
     }
-
 }
+
+export default new TeacherHandler();
