23a0d9611b2efa5436dd4e1837ee36d466040767
unknown <nelsondiasdemedeiros@gmail.com>
2020-01-02 21:21:23 -0200

    feaure: ModuleRepository, patter para desacoplamento do banco de dados

---
+++ backend/src/repositories/module.repository.js
@@ -0,0 +1,18 @@
+import Module from '../models/Module';
+
+class ModuleRepository {
+
+    async create(data) {
+        return await Module.create(data);
+    }
+
+    async getAll() {
+        return await Module.findAll({});
+    }
+
+    async getModule(id) {
+        return await Module.findOne({where: {module_id: id}});
+    }
+}
+
+export default new ModuleRepository();
