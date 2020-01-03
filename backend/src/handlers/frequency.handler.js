3f0d7d8e1a1c71f1d3bfb86b79247dfbd5a3e403
unknown <nelsondiasdemedeiros@gmail.com>
2020-01-03 12:54:33 -0200

    feature: add FrequencyHandler

---
+++ backend/src/handlers/frequency.handler.js
@@ -0,0 +1,40 @@
+import frequencyRepository from '../repositories/frequency.repository';
+import studentRepository from '../repositories/student.repository';
+import classRepository from '../repositories/class.repository';
+
+class FrequencyHandler {
+
+    async store(req, res) {
+
+        try {
+            
+            const student_id = req.params['student_id'];
+            const class_id = req.params['class_id'];
+            
+            const student = await studentRepository.getStudent(student_id);
+            const classs = await classRepository.getClass(class_id);
+
+            if (!student) { throw new Error('STUDENT NOT FOUND'); }
+            if (!classs) { throw new Error('CLASS NOT FOUND'); }
+            
+            req.body['student_id'] = student.student_id;
+            req.body['class_id'] = classs.class_id;
+
+            const frequency = await frequencyRepository.create(req.body);
+
+            return res.status(201).json(frequency);
+
+        } catch (error) {
+            switch (error.message) {
+                case 'STUDENT NOT FOUND': 
+                    return res.status(404).json({error: 'STUDENT NOT FOUND' });
+                case 'CLASS NOT FOUND': 
+                    return res.status(404).json({error: 'CLASS NOT FOUND' });
+                case error.errors:
+                    return res.status(401).json({error: error.errors[0].message });
+            }
+        }
+    }
+}
+
+export default new FrequencyHandler();
