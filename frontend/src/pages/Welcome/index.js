d51e966b0b53199b4208b2718ffe770167c53946
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-08 03:52:21 -0300

    redirect to home if logged in

--- frontend/src/pages/Welcome/index.js
+++ frontend/src/pages/Welcome/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import User from "../../routes/user"
 import Particles from "react-particles-js";
 import "./styles.css";
 
@@ -7,9 +8,12 @@ import Navbar from "../Navbar";
 import CourseName from "./CourseName";
 import About from "./About";
 import Teachers from "./Teachers";
+import redirect from "../../routes/redirect";
 
 
 const Welcome = () => {
+    if (User.isAuthenticated())
+        redirect('/home')
     return (
     <div className="welcome" id="particles-js">
         <Particles
--- frontend/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-// TODO: implements authenticated method
-
-
-const isAuthenticated = () => {
-    return localStorage.getItem('@user/token') ? true : false
-}
-
-
-export default isAuthenticated;
