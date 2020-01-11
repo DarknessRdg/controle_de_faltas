fcdf8c8befc028e2012127d60d4e12f7468a26cf
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-11 17:09:58 -0300

    replace class to className

--- frontend/src/pages/NotAuthenticated/index.js
+++ frontend/src/pages/NotAuthenticated/index.js
@@ -5,7 +5,7 @@ import redirect from '../../routes/redirect';
 
 const NotAuthenticated = () => (
     <div id="not-authenticated">
-        <h4>Desculpe, mas você não tem acesso a esta página. <i class="ml-2 far fa-frown red-text text-darken-1"></i></h4>
+        <h4>Desculpe, mas você não tem acesso a esta página. <i className="ml-2 far fa-frown red-text text-darken-1"></i></h4>
         <h5><a href="#!" className="btn" onClick={() => redirect('/')}>Voltar para o site</a></h5>
     </div>
 )
