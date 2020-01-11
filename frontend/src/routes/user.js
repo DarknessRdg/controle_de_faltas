a9f29f5038f76a16d6326da2dd339c1e1c8556f3
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-11 01:44:25 -0300

    user functions

--- frontend/src/routes/user.js
+++ frontend/src/routes/user.js
@@ -1,17 +1,39 @@
 import redirect from './redirect'
 
 
-const isAuthenticated = () => {
-    return localStorage.getItem('@user/token') ? true : false
+const userToken = '@user/token'
+const userId = '@user/id'
+
+
+function isAuthenticated() {
+    const token = localStorage.getItem(userToken) ? true : false
+    const id = localStorage.getItem(userId) ? true : false
+
+    return token && id
 }
 
 
 function logout() {
-    localStorage.removeItem('@user/token')
-    localStorage.removeItem('@user/id')
+    localStorage.removeItem(userToken)
+    localStorage.removeItem(userId)
     
     redirect('/')
 }
 
 
-export default {isAuthenticated, logout}
+function getToken() {
+    return localStorage.getItem(userToken)
+}
+
+
+function getId() {
+    return localStorage.getItem(userId)
+}
+
+
+function getAtuhorizationHeader() {
+    return  {'Authorization': `Bearer ${getToken()}`}
+}
+
+
+export default {isAuthenticated, logout, getToken, getId, getAtuhorizationHeader}
