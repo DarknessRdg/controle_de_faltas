4d729ddc97d87911285f57ce4c1975d77c71e16f
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-11 16:43:03 -0300

    add data to home

---
+++ frontend/src/pages/Home/Student/index.js
@@ -0,0 +1,29 @@
+import React, {useEffect} from 'react';
+import M from 'materialize-css';
+import './styles.css';
+
+import ClassModules from  '../../../components/ClassModules';
+
+import Arduino from './arduino'
+import Python from './python'
+import Scratch from './scratch'
+
+
+export default () => {
+    useEffect(() => {
+        M.AutoInit();
+    }, [])
+
+    return (<>
+        <div className="p-5">
+            <div id="modules">
+                <h3 className="center">Saiba mais!</h3>
+                <ClassModules modules={[
+                    Arduino,
+                    Python,
+                    Scratch
+                ]} />
+            </div>
+        </div>
+    </>)
+}
