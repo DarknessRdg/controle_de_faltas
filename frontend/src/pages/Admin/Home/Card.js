873c25809b04aeb975aefad7ab4d1cc45f3fad84
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-12 01:58:35 -0300

    home admin

---
+++ frontend/src/pages/Admin/Home/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import redirect from '../../../routes/redirect'
+
+
+export default (props) => {
+    const {data} = props
+
+    return (
+        <div className="card">
+            <div className="card-image">
+                <img className="activator" src={data.img} />
+            </div>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{data.title}</span>
+                <p><a href="#!" onClick={(e) => {redirect(data.redirect, e) }}>Ir para {data.title.toLowerCase()}</a></p>
+            </div>
+        </div>
+    )
+}
---
+++ frontend/src/pages/Admin/Home/data.js
@@ -0,0 +1,47 @@
+import React from 'react'
+const imgPathBase = '../../../assets/img/admin/'
+
+/* eslint-disable import/first */
+import TeachersImg from '../../../assets/img/admin/teachers.png'
+import StudentsImg from '../../../assets/img/admin/students.png'
+import ModulesImg from '../../../assets/img/admin/modules.png'
+import ClassesImg from '../../../assets/img/admin/classes.png'
+import Frequency from '../../../assets/img/admin/frequency.png'
+import RegisterFrequency from '../../../assets/img/admin/frequency-register.png'
+
+
+const cards = [
+    {
+        title: 'Professores',
+        img: TeachersImg,
+        redirect: '/admin/teachers'
+    },
+    {
+        title: 'Alunos',
+        img: StudentsImg,
+        redirect: '/admin/students'
+    },
+    {
+        title: 'Módulos',
+        img: ModulesImg,
+        redirect: '/admin/modules'
+    },
+    {
+        title: 'Aulas',
+        img: ClassesImg,
+        redirect: '/admin/classes'
+    },
+    {
+        title: 'Frequência',
+        img: Frequency,
+        redirect: '/admin/frequecy'
+    },
+    {
+        title: 'Chamada',
+        img: RegisterFrequency,
+        redirect: '/admin/frequency/register'
+    },
+]
+
+
+export default cards
