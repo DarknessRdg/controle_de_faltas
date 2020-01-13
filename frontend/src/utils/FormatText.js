fb7788f23c375f531d6070656afdfe49a01147e7
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-13 04:49:58 -0300

    Fix: FormatText methods

--- frontend/src/utils/FormatText.js
+++ frontend/src/utils/FormatText.js
@@ -1,12 +1,26 @@
-function capitalize(string) {
-    return string
-}
-
-
 function firstUpper(string) {
-    return string[0].toUpperCase() + string.slice(1)
+    if (!string)
+        return ''
+    else if (string.length === 1)
+        return string.toUpperCase()
+    else
+        return string[0].toUpperCase() + string.slice(1)
 }
 
+
+function capitalize(string) {
+    if (!string.includes(' '))
+        return firstUpper(string)
+    
+    let split = string.split(' ')
+    
+    let stringFormated = ''
+    split.forEach(sub => {
+        stringFormated += firstUpper(sub)
+    });
+
+    return stringFormated.trim()
+}
 
 
 export default {
