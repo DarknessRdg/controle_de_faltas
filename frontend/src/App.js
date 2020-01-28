c5dafcaab5af8f00b0b02f24bacd800277606084
Luan Rodrigues <luanrodrigues007@hotmail.com>
2020-01-28 04:08:17 -0300

    add material-icons to package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import './bootstrapClasses.css';
 
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/material-icons/iconfont/material-icons.css';
 
 import Routes from './routes/routes';
 
